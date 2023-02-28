667f966292d866177314b68256f68914ab3d437a
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-02-28 17:30:28 +0100

    import request from flask

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -5,7 +5,7 @@ Create a get_locale function with the babel.localeselector decorator.
 Use request.accept_languages to determine the best match
 with our supported languages..
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
