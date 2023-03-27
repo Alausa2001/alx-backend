9834bfd7493cf883e6614ebaa1bfbb4c3f76c905
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-03-27 15:00:58 +0100

    create a connection to redis

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -2,7 +2,5 @@ import { createClient } from 'redis';
 
 const client = createClient();
 
-client.on('error', err => {
-  if (err) console.log(`Redis client not connected to the server: ${err}`);
-  else console.log('Redis client connected to the server');
-});
+client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
+console.log('Redis client connected to the server');
